a6746ea0a2722affa37b728f3c34bdbe2dbf3d82
Kiler <lucaskiler@gmail.com>
2023-03-08 11:07:00 -0500

    cours11: preparation dossiers pour carrousel

---
+++ carrousel.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Extension carrousel permet d'affichier dans une boite modale les images d'une galerie
+ * Package name: Carrousel
+ * Version 1.0.0
+ */
+
+ /*
+    Plugin name: Carrousel
+    Author: Lucas Kiler
+    Plugin URI: https:github.com/kilerlucas/carrousel 
+    Description: Permet d'affichier dans une boite modale les images d'une galerie avec un systhème de naviagation
+  
